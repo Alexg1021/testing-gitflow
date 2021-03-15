acf356629b02b634e857e0b5d2c1ffd96ab4317e
Anna <annamariej1015@gmail.com>
2021-03-15 14:49:39 -0700

    Annas Component

---
+++ src/AnnasComponent.js
@@ -0,0 +1,9 @@
+const AnnasComponent = () => {
+    return (
+    <div>
+        <h1>AnnaMarie Jimenez</h1>
+    </div>
+    );
+};
+  
+export default AnnasComponent;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AnnasComponent from './AnnasComponent'
 
 const App = () => {
   return (
     <div className='App'>
       <h1>This is my App Component</h1>
       <h3>From my ag-1 feature branch!</h3>
+      <AnnasComponent />
     </div>
   );
 };
