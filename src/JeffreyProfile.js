64303f4c99b1235305780cd6908d50c903ba51fd
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-03-15 15:10:20 -0700

    testing zsh

---
+++ src/JeffreyProfile.js
@@ -0,0 +1,13 @@
+
+
+const JeffreyProfile = () => {
+    return (
+      <div className="App my-5">
+        
+        <h2>Jeffrey Rios JR.</h2>
+        
+      </div>
+    );
+  };
+  
+  export default JeffreyProfile;
