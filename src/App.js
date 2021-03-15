d0f6f9438e13f6c99222b8b2749c0cba0d0b542c
pravallika <pravallikaammineni@github.com>
2021-03-15 14:43:03 -0700

    removed component from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Pravalika from './Pravalika.js';
 
 const App = () => {
   return (
     <div className='App'>
       <h1>This is my App Component</h1>
       <h3>From my ag-1 feature branch!</h3>
-      <Pravalika/>
     </div>
   );
 };
