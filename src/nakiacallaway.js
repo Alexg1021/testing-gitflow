7cfef4f0686eb06c377597f68daf280926312585
Nakia Callaway <nik.callaway@outlook.com>
2021-03-15 14:28:40 -0700

    feat(component): added nakiacallaway.js

---
+++ src/nakiacallaway.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const nikName = () => {
+    return (
+      <div className='row text-center text-primary'>
+        <div className='col'>
+          <h2 className='my-5'>Nakia Callaway</h2>
+        </div>
+      </div>
+    );
+  };
+  
+  export default nikName;   
