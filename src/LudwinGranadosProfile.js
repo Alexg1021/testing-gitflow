3b7da3d30bf8c497f033d628468205ffd8adbc61
Ludwin Granados <ludwingranados@gmail.com>
2021-03-15 14:46:58 -0700

    added LudwinGranadosProfile.js file

---
+++ src/LudwinGranadosProfile.js
@@ -0,0 +1,14 @@
+
+
+
+const LudwinProfileComponent = () => {
+
+    return (
+        <div className='App my-5'></div>
+        <h2>Ludwin Granados</h2>
+        </div>
+    );
+
+};
+
+export default LudwinProfileComponent;
