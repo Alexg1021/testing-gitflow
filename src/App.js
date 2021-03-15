4c202a9ee08041ddac4f80be064e90f79f03a253
Harrsion <hstamps4@gmail.com>
2021-03-15 14:47:22 -0700

    feat(header):title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import HarrisonProfileComponent from './Harrison'
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>This is my App Component</h1>
-      <h3>From my ag-1 feature branch!</h3>
+    <div className='container'>
+     <HarrisonProfileComponent />
     </div>
   );
 };
---
+++ src/Harrison.js
@@ -0,0 +1,11 @@
+const HarrisonProfileComponent = () =>{
+    return (
+        <div className = "row text-center mt-4">
+            <div className ="col">
+                <h2>Harrison</h2>
+            </div>
+        </div>
+    )
+};
+
+export default HarrisonProfileComponent;
