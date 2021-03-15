6b50aafc39f41496ddab633258658cfe8feebb9e
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-03-15 13:52:40 -0700

    refac: needed to update component to arrow func component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <h1>This is my App Component</h1>
       <h3>From my ag-1 feature branch!</h3>
     </div>
   );
-}
+};
 
 export default App;
