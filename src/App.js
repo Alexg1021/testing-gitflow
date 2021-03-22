1b8d5cc324477d72d53b7688211df106ed470d8d
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-03-22 13:31:03 -0700

    profile.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import './App.css';
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div className='container'>
-     <HarrisonProfileComponent />
-=======
     <div className='App'>
       <h1>This is my App Component</h1>
       <h3>From my ag-1 feature branch!</h3>
       
->>>>>>> jr-profile
     </div>
   );
 };
