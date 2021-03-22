d36157a3218a0de850ecfbe7fdbe2de74253cfd2
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-03-22 13:34:55 -0700

    feat(render-profile): importing all team profile components and rendering in app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import AlexProfileComponent from './AlexProfile';
 import LizProfile from './LizProfile';
 import MeruProfile from './MeruProfile';
 import BrittneyComponent from './BrittneyCProfile';
+import NikName from './nakiacallaway';
+import LudwinProfileComponent from './LudwinGranadosProfile';
+import PardeepProfile from './PardeepProfile';
+import Pravalika from './Pravalika';
 
 const App = () => {
   return (
@@ -15,6 +19,10 @@ const App = () => {
       <MeruProfile />
       <BrittneyComponent />
       <HarrisonProfileComponent />
+      <NikName />
+      <LudwinProfileComponent />
+      <PardeepProfile />
+      <Pravalika />
     </div>
   );
 };
