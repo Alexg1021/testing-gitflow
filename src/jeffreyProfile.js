c3441edbda5b4f31a4c3f8140e9832066e4dc6da
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-03-22 13:10:38 -0700

    updated js

---
+++ src/jeffreyProfile.js
@@ -0,0 +1,13 @@
+
+
+const JeffreyProfile = () => {
+    return (
+      <div className="App my-5">
+        
+        <h2>Jeffrey Rios Jr.</h2>
+        
+      </div>
+    );
+  };
+  
+  export default JeffreyProfile;
