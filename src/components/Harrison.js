af606ace74065ee5cc8f5ca4049e15e63a5dc77d
Harrsion <hstamps4@gmail.com>
2021-03-22 15:40:44 -0700

    Updated Harrison Profile with added image personal details

--- src/components/Harrison.js
+++ src/components/Harrison.js
@@ -1,7 +1,39 @@
+import {useState} from 'react';
+import Harrison_profile from '../assets/WIN_20200627_02_34_21_Pro (2).jpg'
 const HarrisonProfileComponent = () =>{
+    const [harrisonProfile, setHarrisonProfile] = useState({
+        name:'Harrison Stamps',
+        role: "Apprenticeship",
+        motto: 'Win at being you!',
+        email: 'hstamps4@gmail.com',
+        hobbies: ["Sports"," coding ", "traveling"]
+    
+    })
     return (
-        
-        <h2>Harrison</h2>
+        <div className="card">
+            <img  src ={Harrison_profile}  
+            className="card-img-top "
+            
+            />
+            <div className="card-body">
+                <h3 className="card-title">{harrisonProfile.name}</h3>
+                <h5 className="card-title text-secondary">{harrisonProfile.role}</h5>
+                <p className="card-text">{harrisonProfile.motto}</p>
+            </div>
+            <div className="list-group list-group-flush">
+                {harrisonProfile.hobbies.map((hobby,index) =>{
+                    return (
+                        <li className="list-group-item" key={index}>
+                            {hobby}
+                        </li>
+                    )
+                })}
+            </div>
+            <div className="card-body">
+                <a href ={harrisonProfile.email} className="card-link">Email</a>
+            </div>
+
+        </div>
     )
 };
 
