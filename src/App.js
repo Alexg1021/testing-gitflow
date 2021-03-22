9bc7535c598ac8d0f96744b4cbc5f5b51f56b59b
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-03-22 10:50:15 -0700

    feat-28(render-profile): not fully complete, waiting on a few more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import AlexProfileComponent from './AlexProfile';
+import LizProfile from './LizProfile';
+import MeruProfile from './MeruProfile';
+import BrittneyComponent from './BrittneyCProfile';
 
 const App = () => {
   return (
     <div className='App'>
       <h1>This is my App Component</h1>
       <h3>From my ag-1 feature branch!</h3>
+      <AlexProfileComponent />
+      <LizProfile />
+      <MeruProfile />
+      <BrittneyComponent />
     </div>
   );
 };
