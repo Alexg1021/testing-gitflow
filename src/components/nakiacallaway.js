502d74a65b730bb5983ae99d903c55a8e4f7ac56
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-03-23 10:02:11 -0700

    bug(img-path): resolving issue with img path in nc.js fil

--- src/components/nakiacallaway.js
+++ src/components/nakiacallaway.js
@@ -1,47 +1,55 @@
-import {useState} from 'react';
-import nakia_profile from '.../assets/nakia.jpg';
+import { useState } from 'react';
+import nakia_profile from '../assets/nakia.jpg';
 
 const NikName = () => {
-    const [nakiaProfile, setNakiaProfile] = useState({
-        name: 'Nakia Callaway',
-        role: 'React Apprentice', 
-        motto: 'Safety Third.',
-        hobbies: ['Woodworking', 'Cooking', 'Gaming'],
-        email: 'nik.callaway@outlook.com', 
-        linkedIn: 'https://www.linkedin.com/in/nakia-callaway-31201b6/',
-        profileImg: nakia_profile,
-    });
-    return (
-      <div className='card'>
-        <img 
-          className="card-img-top" 
-
-          src={nakiaProfile.profileImg} 
-          alt="handsome man with beard wearing purple shirt and vest" 
-        />
-  <div className="card-body text-center">
-      <h3 className="card-title">{nakiaProfile.name}</h3>
-      <h5 className="card-title text-secondary">{nakiaProfile.role}</h5>
-      <p className='card-body'>"{nakiaProfile.motto}"</p>
-    </div>
-  <ul className="list-group list-group-flush">
-      <div className="list-group-item">
-          <strong>HOBBIES:</strong>
+  const [nakiaProfile, setNakiaProfile] = useState({
+    name: 'Nakia Callaway',
+    role: 'React Apprentice',
+    motto: 'Safety Third.',
+    hobbies: ['Woodworking', 'Cooking', 'Gaming'],
+    email: 'nik.callaway@outlook.com',
+    linkedIn: 'https://www.linkedin.com/in/nakia-callaway-31201b6/',
+    profileImg: nakia_profile,
+  });
+  return (
+    <div className='card'>
+      <img
+        className='card-img-top'
+        src={nakiaProfile.profileImg}
+        alt='handsome man with beard wearing purple shirt and vest'
+      />
+      <div className='card-body text-center'>
+        <h3 className='card-title'>{nakiaProfile.name}</h3>
+        <h5 className='card-title text-secondary'>{nakiaProfile.role}</h5>
+        <p className='card-body'>"{nakiaProfile.motto}"</p>
       </div>
-      {nakiaProfile.hobbies.map((hobby, index) => {
+      <ul className='list-group list-group-flush'>
+        <div className='list-group-item'>
+          <strong>HOBBIES:</strong>
+        </div>
+        {nakiaProfile.hobbies.map((hobby, index) => {
           return (
-                <li className="list-group-item" key={index}>
-                    {hobby}
-                </li>
+            <li className='list-group-item' key={index}>
+              {hobby}
+            </li>
           );
-      })};
-  </ul>
-  <div class="card-body">
-  <a href={nakiaProfile.email} class="card-link">Email</a>
-    <a href={nakiaProfile.linkedIn} className="card-link" target="_blank" rel="noreferrer noopener">Linkedin</a>
-  </div>
-</div>
-    );
-  };
-  
-export default NikName;   
+        })}
+        ;
+      </ul>
+      <div class='card-body'>
+        <a href={nakiaProfile.email} class='card-link'>
+          Email
+        </a>
+        <a
+          href={nakiaProfile.linkedIn}
+          className='card-link'
+          target='_blank'
+          rel='noreferrer noopener'>
+          Linkedin
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NikName;
